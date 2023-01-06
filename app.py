b248284ca0ad463103c2c582c10b647ff2597e1e
bonnartech <115790288+bonnartech@users.noreply.github.com>
2023-01-06 23:45:47 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import pickle
+import json
+
+
+app = FastAPI
+origin = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True
+    allow_methods=['*']
+    allow_headers = ["*"]
+)
+
+class model_input(BaseModel):
+    Max-Temprature : float
+    Min-temperature : float
+    Relative humidity 1(0800hrs): float
+    Relative humidity 2(1400hrs): float
+    Rainfall: float
+    mosquito population: int
+    Cases: int
+
+
+malaria_model = pickle.load(open('best_model(1).pkl', 'rb'))
+
+@app.post('/malaria_predictor')
+def malaria_pred(input_parameters: model_input):
+    input_data = input_parameters.json()
+    input_dictionary=json.loads(input_data)
+
+    maxt = input_dictionary['Max-Temperature']
+    mint = input_dictionary['Min-temperature']
+    rel1 = input_dictionary['Relative humidity 1(0800hrs)']
+    rel2 = input_dictionary['Relative humidity 2(1400hr)']
+    rain = input_dictionary['Rainfall']
+    mosqp = input_dictionary['Mosquito population']
+    cas = input_dictionary['Cases']
+
+input_list = [maxt,mint,rel1,rel2,rain,mosqp,cas]
+
+
+if prediction[0]==1:
+    return 'moderate threat'
+else:
+    return 'outbreak'
